5bad13b5378e6929677d14ab340e07c16a32f0ac
Anthony Nguyen <anthony.nguyen@queensu.ca>
2016-08-23 09:47:50 -0400

    Bug fix

--- app/collection/collection.js
+++ app/collection/collection.js
@@ -24,7 +24,7 @@ angular.module('igCollectionApp.collection', ['ngRoute'])
   $http.get('http://instagram-collection-backend.herokuapp.com/api/v1/collections/' + $scope.id).then(function successCallback(response) {
     $scope.collection = response.data;
     $scope.pages = response.data.current_count / 9;
-    if($scope.collection.next_url == "No more") $scope.more = false;
+    if($scope.collection.next_url == "No more" && (($scope.current_page * 9) > (current_count + 9))) $scope.more = false;
   }, function errorCallback(response){
     $location.path("/");
   });
@@ -35,6 +35,7 @@ angular.module('igCollectionApp.collection', ['ngRoute'])
     $http.get('http://instagram-collection-backend.herokuapp.com/api/v1/collections/' + $scope.id + '?page=' + $scope.current_page).then(function successCallback(response) {
       $scope.collection.posts = $scope.collection.posts.concat(response.data.posts);
       $scope.isLoading = false;
+      if($scope.collection.next_url == "No more" && (($scope.current_page * 9) > (current_count + 9))) $scope.more = false;
     }, function errorCallback(response){
       $scope.more = false;
       $scope.isLoading = false;
